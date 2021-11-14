81a2b284928bba1c23e8196a3a51ff2562ef0b2c
Alexey Basalov <6y6jlb@gmail.com>
2021-11-14 20:15:04 +0300

    mongo db added

---
+++ db.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const CONNECTION_TYPE = require("./utils/type");
+
+const dbName = process.env.NAME || '----';
+const dbPass = process.env.PASS || '----';
+const MONGO_DB_URI = `mongodb+srv://${dbName}:${dbPass}@cluster0.ga7l9.mongodb.net/chat_base?retryWrites=true&w=majority`;
+
+
+mongoose.connect(MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+mongoose.connection.on('connected', () => {
+    console.log(chalk.greenBright('data base connected ... .'))
+})
+
+//schema
+const Schema = mongoose.Schema;
+const MessageSchema = new Schema({
+    event: typeof CONNECTION_TYPE,
+    name: String,
+    body: String,
+    date: {
+        type: String,
+        default: Date.now()
+    }
+}, {collection: 'messages_collection'});
+
+//model
+const MessageItem = mongoose.model('Message', MessageSchema);
+
+
+module.exports = MessageItem;
--- index.js
+++ index.js
@@ -4,37 +4,62 @@ const favicon = require('serve-favicon');
 const express = require('express');
 const cors = require('cors')
 const path = require('path')
+const MessageItem = require('./db')
 
 
 const app = express()
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-
 
 const PORT = process.env.PORT || 5000;
 const INDEX = '/index.html';
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .use((req, res) => res.sendFile(INDEX, {root: __dirname}))
     .listen(PORT, () => console.log(chalk.green(`Listening on ${PORT}`)));
 
 
 const wss = new ws.Server({server});
 
-wss.on('connection', connection);
+wss.on('connection', onConnection);
 
-function connection(ws) {
+wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
 
+    return s4() + s4() + '-' + s4();
+};
+
+function onConnection(ws) {
     ws.on('message', (message) => {
-        message = JSON.parse(message);
-        console.log(chalk.bgCyan(JSON.stringify(message)))
+        ws.ws_id = wss.getUniqueID();
+        message = JSON.parse(message,ws.ws_id);
+        console.log(chalk.bgCyan('message', JSON.stringify(message)))
         switch (message.event) {
             case 'message':
+                const newMessage = new MessageItem(message);
+                newMessage.save((error)=>{
+                    if (error) {
+                        console.log(chalk.red('error' , error))
+                    } else {
+                        console.log(chalk.bgGreen('data saved'))
+                    }
+                })
+                broadcastMessage(message)
+                break
             case 'quit':
+                broadcastMessage(message)
+                break
             case 'connection':
+                MessageItem.find({})
+                    .then((data) => {
+                        ws.send(JSON.stringify(data))
+                    })
+                    .catch((err) => {
+                        console.log(chalk.red(err))
+                    })
                 broadcastMessage(message)
                 break
             default:
@@ -44,24 +69,13 @@ function connection(ws) {
 }
 
 function broadcastMessage(message) {
-    message.connectionCounter =  wss.clients.size
-    wss.clients.forEach(client => client.send(JSON.stringify(message)))
+    message.connectionCounter = message.connections =  wss.clients.size
+    wss.clients.forEach(client => client.send(JSON.stringify([message])))
 }
 
 app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
 
-app.get("/*", function(req, res) {
+app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 })
-
-// app.get('/ping', (req, res) => {
-//     res.send('bot here')
-// });
-//
-// app.listen(PORT, () => {
-//     console.log(`Example app listening at http://localhost:${PORT}`)
-// });
-
-
-
 
