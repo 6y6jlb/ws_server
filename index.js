42a56f33c25cfe18387b03c2b7fe736d7bf466b8
Alexey Basalov <6y6jlb@gmail.com>
2021-12-12 18:41:14 +0300

    reg/auth/refactoring

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = express()
     .use(cookieParser())
     .use(cors({
         credentials: true,
-        origin: 'https://6y6jlb.github.io/'
+        origin: 'http://localhost:3000'
     }))
     .use(express.static(path.join(__dirname, 'client/build')))
     .use(express.json())
