70ad1296685a98e836619feb3d2b663963bd20aa
Alexey Basalov <6y6jlb@gmail.com>
2021-12-26 15:20:32 +0300

    added icon url

---
+++ http/weatherConst.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+const weather = {
+    weatherUrl:'data/2.5/weather/',
+    iconPrefix:'img/wn/',
+    iconPostfix:'@2x.png',
+    baseURL:'https://api.openweathermap.org/',
+    baseIconURL:'https://openweathermap.org/',
+};
+
+module.exports = weather;
