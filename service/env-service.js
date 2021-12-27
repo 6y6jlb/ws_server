74ba73801ea7d7138734a92fa8aedc33fa79e8ca
Alexey Basalov <6y6jlb@gmail.com>
2021-12-27 07:42:12 +0300

    refactoring

--- service/env-service.js
+++ service/env-service.js
@@ -5,7 +5,7 @@ const weather_api_key = process.env.WEATHER_API_KEY || '====';
 
 
 class EnvService {
-    async getWeather() {
+    async getWeather(city='moscow') {
        const weatherObj =  await weatherInstance.get(`${weather.weatherUrl}?q=${'moscow'}&appid=${weather_api_key}`);
        const iconUrl = `${weather.baseIconURL}${weather.iconPrefix}${weatherObj.data.weather[0].icon}${weather.iconPostfix}`;
         return {...weatherObj,data: {...weatherObj.data,weather: {...weatherObj.data.weather,icon:iconUrl}}};
