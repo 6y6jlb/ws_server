af6978592ee2a054dac5c4e54ba60075a1c773cd
AlexB <6y6jlb@gmail.com>
2022-01-05 13:50:33 +0300

    refactoring

--- mongoose/config.js
+++ mongoose/config.js
@@ -1,8 +1,8 @@
-const dbName = process.env.NAME || '===';
-const dbPass = process.env.PASS || '===';
+const dbName = process.env.NAME || '6y6jlb';
+const dbPass = process.env.PASS || 'a6s7e8811';
 
 module.exports = {
-    secret : process.env.KEY || '===',
+    secret : process.env.KEY || 'SECRET_KEY_666',
     MONGO_DB_URI : `mongodb+srv://${dbName}:${dbPass}@cluster0.ga7l9.mongodb.net/chat_base?retryWrites=true&w=majority`,
     refreshTokenAge: 2592000000 //30d
 }
