d7b00ac1a1e8e06ca6c1640dfd218e3337a0f6b2
AlexB <6y6jlb@gmail.com>
2022-01-16 17:52:55 +0300

    refactoring weather

--- controllers/envController.js
+++ controllers/envController.js
@@ -5,7 +5,12 @@ const envService = require('../service/env-service');
 class EnvController {
     async getWeather(req, res, next) {
         try {
-           const weatherObject = await envService.getWeather();
+            const requestObj = {
+                    language:req.body.language.toLowerCase(),
+                    location:{
+                        country:req.body.location.country.slice(0,2).toUpperCase(),city:req.body.location.city}};
+            console.log(requestObj);
+            const weatherObject = await envService.getWeather(requestObj);
             return res.status(200).json(weatherObject.data);
         } catch (e) {
             next(e);
--- service/env-service.js
+++ service/env-service.js
@@ -1,14 +1,21 @@
-
 const weatherInstance = require('../http/weatherInstance')
 const weather = require('../http/weatherConst')
-const weather_api_key = process.env.WEATHER_API_KEY || '====';
+const weather_api_key = process.env.WEATHER_API_KEY || '===';
 
 
 class EnvService {
-    async getWeather(city='moscow') {
-       const weatherObj =  await weatherInstance.get(`${weather.weatherUrl}?q=${'moscow'}&appid=${weather_api_key}`);
-       const iconUrl = `${weather.baseIconURL}${weather.iconPrefix}${weatherObj.data.weather[0].icon}${weather.iconPostfix}`;
-        return {...weatherObj,data: {...weatherObj.data,weather: {...weatherObj.data.weather,icon:iconUrl}}};
+    async getWeather(payload = {language : 'ru', location : {country: 'RU', city: 'moskow',}}) {
+        console.log(payload)
+        const params = `q=${payload.location.city},${payload.location.country.toLowerCase()}&appid=${weather_api_key}&lang=${payload.language}&units=metric`
+        console.log(`
+            ${weather.weatherUrl}
+            ?${params}`);
+        const weatherObj = await weatherInstance.get(
+            `${weather.weatherUrl}?${params}`
+        );
+        const iconUrl = `${weather.baseIconURL}${weather.iconPrefix}${weatherObj.data.weather[0].icon}${weather.iconPostfix}`;
+        return {...weatherObj, data: {...weatherObj.data, weather: {...weatherObj.data.weather, icon: iconUrl}}};
     }
 }
+
 module.exports = new EnvService()
